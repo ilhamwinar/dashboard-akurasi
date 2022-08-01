0e53d2056d273279fb87e7d5383290d9e5b551e5
ilhamwinar <ilhamwinarnugroho@gmail.com>
2022-08-02 01:15:19 +0700

    add drop down gerbang

--- dashboard.py
+++ dashboard.py
@@ -7,16 +7,16 @@ import base64
 from io import BytesIO
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import numpy as np
 
 def convert_to_utc(dt_str):
     Format  = "%Y-%m-%d"
     Format_iso="%Y-%m-%d"
     local_dt = datetime.strptime(dt_str, Format)
-    print('Datetime in Local Time zone: ', local_dt)
+    # print('Datetime in Local Time zone: ', local_dt)
     dt_utc = local_dt.astimezone(pytz.UTC).isoformat()
-    print('Datetime in UTC Time zone: ', dt_utc)
-    print(type(dt_utc))
+    # print('Datetime in UTC Time zone: ', dt_utc)
+    # print(type(dt_utc))
     return dt_utc
 
 def convert_to_utc_hari(dt_str):
@@ -25,12 +25,12 @@ def convert_to_utc_hari(dt_str):
     local_dt = datetime.strptime(start, "%Y-%m-%d")
     td = timedelta(hours=+23, minutes=+59)
     end_dt=local_dt+td
-    print('Datetime in start Time zone: ', local_dt)
-    print('Datetime in end Time zone: ', end_dt)
+    # print('Datetime in start Time zone: ', local_dt)
+    # print('Datetime in end Time zone: ', end_dt)
     start_dt_utc = local_dt.astimezone(pytz.UTC).isoformat()
     end_dt_utc = end_dt.astimezone(pytz.UTC).isoformat()
-    print('Datetime in UTC start Time zone: ', start_dt_utc)
-    print('Datetime in UTC end Time zone: ', end_dt_utc)
+    # print('Datetime in UTC start Time zone: ', start_dt_utc)
+    # print('Datetime in UTC end Time zone: ', end_dt_utc)
     return start_dt_utc,end_dt_utc
 
 def shift3(dt_str_start,dt_str_end):
@@ -39,19 +39,144 @@ def shift3(dt_str_start,dt_str_end):
     end= dt_str_end
     local_dt = datetime.strptime(start, "%Y-%m-%d %H-%M-%S")
     end_dt = datetime.strptime(end, "%Y-%m-%d %H-%M-%S")
-    print('Datetime in start Time zone: ', local_dt)
-    print('Datetime in end Time zone: ', end_dt)
+    # print('Datetime in start Time zone: ', local_dt)
+    # print('Datetime in end Time zone: ', end_dt)
     start_dt_utc = local_dt.astimezone(pytz.UTC).isoformat()
     end_dt_utc = end_dt.astimezone(pytz.UTC).isoformat()
-    print('Datetime in UTC start Time zone: ', start_dt_utc)
-    print('Datetime in UTC end Time zone: ', end_dt_utc)
+    # print('Datetime in UTC start Time zone: ', start_dt_utc)
+    # print('Datetime in UTC end Time zone: ', end_dt_utc)
     return start_dt_utc,end_dt_utc
 
 
 st.set_page_config(layout="wide")
 
 st.title ("AKURASI AVC DASHBOARD")
-ip = st.text_input("Masukan IP")
+st.text('Visualisasi data akurasi AVC Tahap 2, dengan memilih kode gerbang, didapatkan hasil akurasi AVC jika dibandingkan dengan data store (Data Transaksi) GTO. ')
+
+option = st.selectbox(
+     'Masukan Nama Gerbang!',
+     ('Cikunir4_5', 'Cikunir4_6', 'Cikunir2_5',
+     'Cikunir2_6', 'Cikunir2_7', 'Cakung1_3',
+     'Cakung1_4','Bintara 3','Bintara 4',
+     'PGB1','Bekasi_Barat16','Bekasi_Barat18',
+     'Karawang_barat1_1','Karawang_barat1_3','Karawang_timur2_10',
+     'Kalihurip1_8','Kalihurip1_10','Kalihurip2_1',
+     'Kalihurip2_3','Cikatama1_1','Cikatama1_3',
+     'Cikatama1_5','Cikatama1_7','Cikatama5_35',
+     'Cikatama5_37','Cikatama5_39','Cikatama5_41',
+     'Karawang_Barat2_13ON','Karawang_Barat2_13OFF','GunungPutri_5',
+     'GunungPutri_6','GunungPutri_7','GunungPutri_8',
+     'Citereup1_6','Citereup1_8','Citereup2_1','Citereup2_3',
+     'Sentul1_6','Sentul1_8','Sentul2_1','Sentul2_3',
+     'Sentul_selatan_5','Sentul_selatan_7',
+     'Ciawi1_12','Ciawi1_14','Ciawi1_16','Ciawi1_18',
+     'Ciawi2_11','Ciawi2_13','Ciawi2_15',
+     ))
+
+if option == 'Cikunir4_5':  
+    ip='10.0.3.2'     
+if option == 'Cikunir4_6':  
+    ip='10.0.4.2'
+if option == 'Cikunir2_5':  
+    ip='10.0.5.2'
+if option == 'Cikunir2_6':  
+    ip='10.0.6.2'
+if option == 'Cikunir2_7':
+    ip='10.0.7.2'
+if option == 'Cakung1_3':
+    ip='10.0.8.2'
+if option == 'Cakung1_4':
+    ip='10.0.9.2'
+if option == 'Bintara 3':
+    ip='10.0.53.2'
+if option == 'Bintara 4':
+    ip='10.0.54.2'
+if option == 'PGB1':
+    ip='10.0.12.2'
+if option == 'Bekasi_Barat16':
+    ip='10.0.13.2'
+if option == 'Bekasi_Barat18':
+    ip='10.0.14.2'
+if option == 'Karawang_barat1_1':
+    ip='10.0.15.2'
+if option == 'Karawang_barat1_3':
+    ip='10.0.16.2'
+if option == 'Karawang_timur2_10':
+    ip='10.0.17.2'
+if option == 'Kalihurip1_8':
+    ip='10.0.18.2'
+if option == 'Kalihurip1_10':
+    ip='10.0.19.2'
+if option == 'Kalihurip2_1':
+    ip='10.0.20.2'
+if option == 'Kalihurip2_3':
+    ip='10.0.21.2'
+if option == 'Cikatama1_1':
+    ip='10.0.22.2'
+if option == 'Cikatama1_3':
+    ip='10.0.23.2'
+if option == 'Cikatama1_5':
+    ip='10.0.24.2'
+if option == 'Cikatama1_7':
+    ip='10.0.25.2'
+if option == 'Cikatama5_35':
+    ip='10.0.26.2'
+if option == 'Cikatama5_37':
+    ip='10.0.27.2'
+if option == 'Cikatama5_39':
+    ip='10.0.28.2'
+if option == 'Cikatama5_41':
+    ip='10.0.29.2'
+if option == 'Karawang_Barat2_13ON':
+    ip='10.0.51.2'
+if option == 'Karawang_Barat2_13OFF':
+    ip='10.0.52.2'
+if option == 'GunungPutri_5':
+    ip='172.20.6.125'
+if option == 'GunungPutri_6':
+    ip='172.20.6.126'
+if option == 'GunungPutri_7':
+    ip='172.20.6.127'
+if option == 'GunungPutri_8':
+    ip='172.20.6.128'
+if option == 'Citereup1_6':
+    ip='172.20.5.126'
+if option == 'Citereup1_8':
+    ip='1172.20.5.128'
+if option == 'Citereup2_1':
+    ip='172.20.5.121'
+if option == 'Citereup2_3':
+    ip='172.20.5.123'
+if option == 'Sentul1_6':
+    ip='172.20.4.126'
+if option == 'Sentul1_8':
+    ip='1172.20.4.128'
+if option == 'Sentul2_1':
+    ip='172.20.4.121'
+if option == 'Sentul2_3':
+    ip='172.20.4.123'
+if option == 'Sentul_selatan_5':
+    ip='172.20.3.125'
+if option == 'Sentul_selatan_7':
+    ip='172.20.3.127'
+if option == 'Ciawi1_12':
+    ip='172.20.1.132'
+if option == 'Ciawi1_14':
+    ip='172.20.1.134'
+if option == 'Ciawi1_16':
+    ip='172.20.1.136'
+if option == 'Ciawi1_18':
+    ip='172.20.1.138'
+if option == 'Ciawi2_11':
+    ip='172.20.16.131'
+if option == 'Ciawi2_13':
+    ip='172.20.16.133'
+if option == 'Ciawi2_15':
+    ip='172.20.16.135'
+
+
+# ip = st.text_input("Masukan IP")
+print(type(ip))
 httpserver = "http://"+ip+":8080"
 
 today = datetime.today()
@@ -61,8 +186,6 @@ try:
     client = DataFrameClient(ip, 8086, '', '', 'avc')
     start = start_date.strftime("%Y-%m-%d")
     end = end_date.strftime("%Y-%m-%d")
-
-
     start_time=convert_to_utc(start)
     end_time=convert_to_utc(end)
     # Query String per Hari 
@@ -77,15 +200,17 @@ try:
     # df['gol_avc'] = df['gol_avc'].replace([0],1)
     cam1=[]
     for row,koreksi in zip(df.itertuples(),range(len(df))):
-        print(row)
+        # print(row)
         cam = httpserver+row.cam1_location.replace("..", "")
-        print(cam)
+        # print(cam)
         cam1.append(cam)
     df['cam1']=cam1
     st.write(df)
-    
-except:
+
+except (NameError):
+    print("error")
     pass
+
 
 def get_table_download_link_csv(df):
     #csv = df.to_csv(index=False)
@@ -97,97 +222,39 @@ def get_table_download_link_csv(df):
 
 # df = df # your dataframe
 st.markdown(get_table_download_link_csv(df), unsafe_allow_html=True)
-st.title('Akurasi Matrix')
+st.title('Akurasi Total')
 
 # Parse AI data from Cam Location
-gol_avc_ai = df['cam1_location'].str[26:27].replace("0", "1")
+df['gol_avc_ai'] = df['gol_avc'].replace("6", "1")
+
 # Add Gol AI to Data Frame 
-df['gol_avc_ai'] = gol_avc_ai
-# Combine AI and PulTol
-gol_kombinasi = df['gol_trans'].apply(str) + df['cam1_location'].str[26:27].replace("0", "1").replace("8", "1")
-# Add Combine Gol to Data Frame
-df['gol_kombinasi'] = gol_kombinasi
-# Count Combine Gol
-gol_11 = df[df.gol_kombinasi=='11'].count()["gol_kombinasi"]
-gol_12 = df[df.gol_kombinasi=='12'].count()["gol_kombinasi"]
-gol_13 = df[df.gol_kombinasi=='13'].count()["gol_kombinasi"]
-gol_14 = df[df.gol_kombinasi=='14'].count()["gol_kombinasi"]
-gol_15 = df[df.gol_kombinasi=='15'].count()["gol_kombinasi"]
-gol_21 = df[df.gol_kombinasi=='21'].count()["gol_kombinasi"]
-gol_22 = df[df.gol_kombinasi=='22'].count()["gol_kombinasi"]
-gol_23 = df[df.gol_kombinasi=='23'].count()["gol_kombinasi"]
-gol_24 = df[df.gol_kombinasi=='24'].count()["gol_kombinasi"]
-gol_25 = df[df.gol_kombinasi=='25'].count()["gol_kombinasi"]
-gol_31 = df[df.gol_kombinasi=='31'].count()["gol_kombinasi"]
-gol_32 = df[df.gol_kombinasi=='32'].count()["gol_kombinasi"]
-gol_33 = df[df.gol_kombinasi=='33'].count()["gol_kombinasi"]
-gol_34 = df[df.gol_kombinasi=='34'].count()["gol_kombinasi"]
-gol_35 = df[df.gol_kombinasi=='35'].count()["gol_kombinasi"]
-gol_41 = df[df.gol_kombinasi=='41'].count()["gol_kombinasi"]
-gol_42 = df[df.gol_kombinasi=='42'].count()["gol_kombinasi"]
-gol_43 = df[df.gol_kombinasi=='43'].count()["gol_kombinasi"]
-gol_44 = df[df.gol_kombinasi=='44'].count()["gol_kombinasi"]
-gol_45 = df[df.gol_kombinasi=='45'].count()["gol_kombinasi"]
-gol_51 = df[df.gol_kombinasi=='51'].count()["gol_kombinasi"]
-gol_52 = df[df.gol_kombinasi=='52'].count()["gol_kombinasi"]
-gol_53 = df[df.gol_kombinasi=='53'].count()["gol_kombinasi"]
-gol_54 = df[df.gol_kombinasi=='54'].count()["gol_kombinasi"]
-gol_55 = df[df.gol_kombinasi=='55'].count()["gol_kombinasi"]
+df= df[df['gol_avc_ai'] != 0]
 
-# Create Data Frame Confussion Matrix
-df_confusion_matrix = pd.DataFrame({'1': [gol_11, gol_21, gol_31, gol_41, gol_51],'2': [gol_12, gol_22, gol_32, gol_42, gol_52],'3': [gol_13, gol_23, gol_33, gol_43, gol_53],'4': [gol_14, gol_24, gol_34, gol_44, gol_54],'5': [gol_15, gol_25, gol_35, gol_45, gol_55]})
-df_confusion_matrix.index = df_confusion_matrix.index + 1
-st.dataframe(df_confusion_matrix)
+actual=list(df['gol_etoll'])
+pred=list(df['gol_avc_ai'])
+y_true_all= pd.Series(actual)
+y_pred_all= pd.Series(pred)
 
-# Count Total Gol
-total_gol_1 = gol_11 + gol_12 + gol_13 + gol_14 + gol_15
-total_gol_2 = gol_21 + gol_22 + gol_23 + gol_24 + gol_25
-total_gol_3 = gol_31 + gol_32 + gol_33 + gol_34 + gol_35
-total_gol_4 = gol_41 + gol_42 + gol_43 + gol_44 + gol_45
-total_gol_5 = gol_51 + gol_52 + gol_53 + gol_54 + gol_55
-
-# Count Percentage
-percentage_gol_11 = (df[df.gol_kombinasi=='11'].count()["gol_kombinasi"]/total_gol_1)*100
-percentage_gol_12 = (df[df.gol_kombinasi=='12'].count()["gol_kombinasi"]/total_gol_1)*100
-percentage_gol_13 = (df[df.gol_kombinasi=='13'].count()["gol_kombinasi"]/total_gol_1)*100
-percentage_gol_14 = (df[df.gol_kombinasi=='14'].count()["gol_kombinasi"]/total_gol_1)*100
-percentage_gol_15 = (df[df.gol_kombinasi=='15'].count()["gol_kombinasi"]/total_gol_1)*100
-percentage_gol_21 = (df[df.gol_kombinasi=='21'].count()["gol_kombinasi"]/total_gol_2)*100
-percentage_gol_22 = (df[df.gol_kombinasi=='22'].count()["gol_kombinasi"]/total_gol_2)*100
-percentage_gol_23 = (df[df.gol_kombinasi=='23'].count()["gol_kombinasi"]/total_gol_2)*100
-percentage_gol_24 = (df[df.gol_kombinasi=='24'].count()["gol_kombinasi"]/total_gol_2)*100
-percentage_gol_25 = (df[df.gol_kombinasi=='25'].count()["gol_kombinasi"]/total_gol_2)*100
-percentage_gol_31 = (df[df.gol_kombinasi=='31'].count()["gol_kombinasi"]/total_gol_3)*100
-percentage_gol_32 = (df[df.gol_kombinasi=='32'].count()["gol_kombinasi"]/total_gol_3)*100
-percentage_gol_33 = (df[df.gol_kombinasi=='33'].count()["gol_kombinasi"]/total_gol_3)*100
-percentage_gol_34 = (df[df.gol_kombinasi=='34'].count()["gol_kombinasi"]/total_gol_3)*100
-percentage_gol_35 = (df[df.gol_kombinasi=='35'].count()["gol_kombinasi"]/total_gol_3)*100
-percentage_gol_41 = (df[df.gol_kombinasi=='41'].count()["gol_kombinasi"]/total_gol_4)*100
-percentage_gol_42 = (df[df.gol_kombinasi=='42'].count()["gol_kombinasi"]/total_gol_4)*100
-percentage_gol_43 = (df[df.gol_kombinasi=='43'].count()["gol_kombinasi"]/total_gol_4)*100
-percentage_gol_44 = (df[df.gol_kombinasi=='44'].count()["gol_kombinasi"]/total_gol_4)*100
-percentage_gol_45 = (df[df.gol_kombinasi=='45'].count()["gol_kombinasi"]/total_gol_4)*100
-percentage_gol_51 = (df[df.gol_kombinasi=='51'].count()["gol_kombinasi"]/total_gol_5)*100
-percentage_gol_52 = (df[df.gol_kombinasi=='52'].count()["gol_kombinasi"]/total_gol_5)*100
-percentage_gol_53 = (df[df.gol_kombinasi=='53'].count()["gol_kombinasi"]/total_gol_5)*100
-percentage_gol_54 = (df[df.gol_kombinasi=='54'].count()["gol_kombinasi"]/total_gol_5)*100
-percentage_gol_55 = (df[df.gol_kombinasi=='55'].count()["gol_kombinasi"]/total_gol_5)*100
-
-# Create Data Frame Confussion Matrix Percentage
-df_confusion_matrix_percentage = pd.DataFrame({'1': [percentage_gol_11, percentage_gol_21, percentage_gol_31, percentage_gol_41, percentage_gol_51],'2': [percentage_gol_12, percentage_gol_22, percentage_gol_32, percentage_gol_42, percentage_gol_52],'3': [percentage_gol_13, percentage_gol_23, percentage_gol_33, percentage_gol_43, percentage_gol_53],'4': [percentage_gol_14, percentage_gol_24, percentage_gol_34, percentage_gol_44, percentage_gol_54],'5': [percentage_gol_15, percentage_gol_25, percentage_gol_35, percentage_gol_45, percentage_gol_55]})
-# Add Data Frame Index + 1
-df_confusion_matrix_percentage.index = df_confusion_matrix_percentage.index + 1
+tab=pd.crosstab(y_true_all, y_pred_all,rownames=['GTO'], colnames=['AVC'])
+akurasi=np.diag(tab).sum() / tab.to_numpy().sum()
+error =1-akurasi
+st.success("Accuracy: " +str(akurasi*100)+" %")
+st.error("Accuracy: " +str(error*100)+" %")
+print("Akurasi: "+ str(akurasi))
+print("error: "+ str(error))
 
 
-st.dataframe(df_confusion_matrix_percentage)
-total = total_gol_1 + total_gol_2 + total_gol_3 + total_gol_4 + total_gol_5
-ok = gol_11 + gol_22 + gol_33 + gol_44 + gol_55
-error = total - ok
-accuracy = (ok/total)*100
-error = (error/total)*100
+st.title('Akurasi Confution Matrix')
+fig = plt.figure(figsize=(4, 2))
+sns.set(font_scale=0.6)
+sns.heatmap(tab,label=['AVC', 'GTO'], annot=True,fmt='g', cmap='Blues')
+st.pyplot(fig)
 
-st.success("Accuracy: " +str(accuracy)+" %")
-st.error("Accuracy: " +str(error)+" %")
+confusion_matrix_pcntg = pd.crosstab(y_true_all, y_pred_all, rownames=['GTO'], colnames=['AVC'], normalize='index')
+fig2=plt.figure(figsize=(4, 2))
+# sns.set(font_scale=1.5)
+sns.heatmap(confusion_matrix_pcntg, cmap='rocket_r', annot=True, fmt=".2%")
+st.pyplot(fig2)
 
 
 
